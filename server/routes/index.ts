f0ef90dc5e7f1b5523f7ea141dea9d42ffac7448
Daniel Roe <daniel@roe.dev>
2025-01-13 10:23:13 +0000

    chore: add reproduction

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,3 +1,3 @@
-export default eventHandler((event) => {
-  return "Start by editing <code>server/routes/index.ts</code>.";
+export default eventHandler(async (event) => {
+  return await useStorage().getKeys()
 });
