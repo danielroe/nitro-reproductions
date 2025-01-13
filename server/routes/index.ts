0abe2613e2afd09877b1c37e9b6a0c83f3e37ecf
Daniel Roe <daniel@roe.dev>
2025-01-13 10:20:02 +0000

    chore: init

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,3 +1,3 @@
-export default eventHandler(async (event) => {
-  return await useStorage().getKeys()
+export default eventHandler((event) => {
+  return "Start by editing <code>server/routes/index.ts</code>.";
 });
